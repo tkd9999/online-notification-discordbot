b3da0ee51b529dfb24051927afeed0466817f86b
junshintakeda <junshintakeda12@gmail.com>
2023-10-15 19:34:04 +0900

    Fixed mobile-online update

--- main.go
+++ main.go
@@ -6,15 +6,18 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
 
 var statushistory map[string]bool
+var mobilehistory map[string]time.Time
 
 func init() {
 	statushistory = make(map[string]bool)
+	mobilehistory = make(map[string]time.Time)
 }
 
 func main() {
@@ -35,6 +38,31 @@ func main() {
 	// _, err = dg.ChannelMessageSend(os.Getenv("CHANNEL_ID"), "Bot is online!")
 	// if err != nil {
 	// 	log.Println("Error sending startup message:", err)
+	// }
+
+	// members, err := dg.GuildMembers(os.Getenv("GUILD_ID"), "", 1000)
+	// if err != nil {
+	// 	log.Println("Error getting members:", err)
+	// }
+
+	// guild, err := dg.Guild(os.Getenv("GUILD_ID"))
+	// if err != nil {
+	// 	log.Println("Error getting guild:", err)
+	// }
+
+	// onlinemembers := make([]string, 0)
+	// for _, member := range members {
+	// 	if status, _ := guild.GetPresence(member.User.ID).ClientStatus.Desktop; status == discordgo.StatusOnline {
+	// 		onlinemembers = append(onlinemembers, member.User.Username)
+	// 	}
+	// }
+
+	// if len(onlinemembers) > 0 {
+	// 	onlinemembersstring := strings.Join(onlinemembers, " ")
+	// 	_, err = dg.ChannelMessageSend(os.Getenv("CHANNEL_ID"), fmt.Sprintf("がオンラインです: %s", onlinemembersstring))
+	// 	if err != nil {
+	// 		log.Println("Error sending startup message:", err)
+	// 	}
 	// }
 
 	dg.AddHandler(onlinenotification)
@@ -68,7 +96,11 @@ func onlinenotification(s *discordgo.Session, m *discordgo.PresenceUpdate) {
 				log.Println("Error sending message:", err)
 			}
 		} else if m.Presence.ClientStatus.Mobile == discordgo.StatusOnline {
-			_, err := s.ChannelMessageSend(os.Getenv("CHANNEL_ID"), fmt.Sprintf("%sが偵察しています", user.Username))
+			if time.Now().Sub(mobilehistory[m.User.ID]).Minutes() < 5 {
+				return
+			}
+			mobilehistory[m.User.ID] = time.Now()
+			_, err := s.ChannelMessageSend(os.Getenv("CHANNEL_ID"), fmt.Sprintf("%sがオンラインです(モバイル)", user.Username))
 			if err != nil {
 				log.Println("Error sending message:", err)
 			}
